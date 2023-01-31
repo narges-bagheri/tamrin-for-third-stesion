208cb820a1c833660a4b3d0f3ffbaf32e50e72a0
narges-bagheri <111405499+narges-bagheri@users.noreply.github.com>
2023-01-31 16:51:07 +0330

    Add files via upload

---
+++ tamrin 3 stesion/three.js
@@ -0,0 +1,18 @@
+const myConst1=+prompt('Enter first number : ');
+const myConst2=+prompt('Enter second number : ');
+var sum=(myConst1+myConst2);
+console.log('sum is : '+sum);
+var div=(myConst1/myConst2);
+console.log('division is : '+div);
+var percent=(myConst1%myConst2);
+console.log('percent is : '+percent);
+var multiply=(myConst1*myConst2);
+console.log('multiply is : '+multiply);
+
+
+var age=+prompt('enter youur age plz : ');
+if (age<18){
+    alert ('you are not allowable ! ')
+}else{
+    alert ('you are allowable ! ')
+}
